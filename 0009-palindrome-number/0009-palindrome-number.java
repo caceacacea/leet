b54c4c7b77be26a14fd87f189be6056ea134775e
cacea <sagidudewhynot@gmail.com>
2026-05-13 10:02:03 +0900

    Time: 7 ms (14.11%), Space: 46.1 MB (17.54%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -5,12 +5,11 @@ class Solution {
 		boolean isP = true;
 		
 		for (int i = 0; i <=  isPal.length()/2; i++) {
-			if (isPal.charAt(i) == isPal.charAt(isPal.length() - i - 1)) {
-			} else {
+			if (isPal.charAt(i) != isPal.charAt(isPal.length() - i - 1)) {
 				isP = false;
 			}
 		}
 		
 		return isP;
-    }
+	}
 }
